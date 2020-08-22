2b234606f99234e841451f37bea66e83147b9ee4
ElizabethdaLuz <70077984+ElizabethdaLuz@users.noreply.github.com>
2020-08-22 20:21:12 -0300

    Create Programa3.c

---
+++ Microcontroladores- 2020-2/Prática01/Programa3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+  int n1, n2, S;
+
+  printf("Informe um numero:\n");
+  scanf("%i", &n1);
+  printf("Informe outro numero:\n");
+  scanf("%i", &n2);
+  S= n1+n2;
+  printf("Resultado: %i\n", S);
+   return 0;
+}
