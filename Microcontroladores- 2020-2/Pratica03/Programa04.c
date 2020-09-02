dd1248b3acecec97a4878d7b2ee6f9da508faf58
ElizabethdaLuz <70077984+ElizabethdaLuz@users.noreply.github.com>
2020-09-02 09:37:52 -0300

    Create Programa04.c

---
+++ Microcontroladores- 2020-2/Pratica03/Programa04.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+float converterParaRadiano(float graus){
+  float resposta;
+  resposta = graus *M_PI/ 180;
+  return resposta;
+}
+float calcularSeno(float angulo){
+  float resposta;
+  angulo = converterParaRadiano(angulo);
+  resposta = sin(angulo);
+  return resposta;
+}
+int main(void) {
+  float angulo;
+  printf("informe o valor do angulo:");
+  scanf("%f", &angulo);
+  printf("O valor de %f em radianos eh %f\n", angulo, converterParaRadiano(angulo));
+
+  return 0;
+}
