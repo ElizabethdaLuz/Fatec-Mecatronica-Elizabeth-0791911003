89f2ca9fa5e0a5ed03aed2f58cce9e53c74f9504
ElizabethdaLuz <70077984+ElizabethdaLuz@users.noreply.github.com>
2020-09-08 21:26:12 -0300

    Update Programa01.c

--- Microcontroladores- 2020-2/Pratica02/Programa01.c
+++ Microcontroladores- 2020-2/Pratica02/Programa01.c
@@ -1,32 +1,12 @@
 
 #include <stdio.h>
 
-int main(void){
-  float temperaturas[3];
-  //Ler as 3 temperaturas
-  int i;
-  for(i = 0; i < 3; i++){
-    printf("Informe a temperatura %i:", i);
-    scanf("%f",&temperaturas[i]);
-  }
-
-  float soma;
-  soma = 0;
-  for(i = 0; i < 3; i++){
-    soma = soma + temperaturas[i];
-  }
-
-  float media = soma / i;
-
-  printf("Media dos valores: %f\n", media);
-
-  for (i=0; i<3; i++){
-       if (temperaturas[i] < media)
-         printf("%f eh menor que a media\n", temperaturas[i]);
-       else if (temperaturas[i] > media){
-       printf("%f eh maior que a media\n" , temperaturas[i]);
-  }else 
-  printf ("%f eh igual a media\n", temperaturas[i]);}
-  
+int main(void) {
+  float A,B,X,y;
+  printf("Informe os valores de A,B e X:");
+  // scanf("%f", &A);
+  scanf("%f%f%f", &A, &B, &X);
+  y = (A*X) + B;
+  printf("Resultado da equacao: %.3f\n", y);
   return 0;
 }
